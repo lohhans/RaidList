d9ffdc61dbfb56a98fe2d2b71afdedd06c321cf7
Armstrong Lohãns <lohansdemelo1108@gmail.com>
2020-12-08 03:09:59 -0300

    Hatched menu started

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Raid list',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -26,22 +29,13 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Lista de Reide'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +44,179 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  bool _chocou = false;
+  double _remainTimeOfRaid = 1;
+  double _maxWaitingTimeForRaid = 1;
+  var _currentTime = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: <Widget>[
+              Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Monte sua lista para a Reide:',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 24,
+                          ),
+                          Text(
+                            'Digite o nome do Pokémon:',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextFormField(),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'O ovo já chocou?',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          Center(
+                            child: ToggleSwitch(
+                              initialLabelIndex: _chocou ? 1 : 0,
+                              minWidth: 90.0,
+                              cornerRadius: 20.0,
+                              activeBgColor: Colors.red,
+                              activeFgColor: Colors.white,
+                              inactiveBgColor: Colors.grey,
+                              inactiveFgColor: Colors.white,
+                              labels: ['SIM', 'NÃO'],
+                              icons: [
+                                FontAwesomeIcons.check,
+                                FontAwesomeIcons.times,
+                              ],
+                              onToggle: (_) {
+                                setState(() {
+                                  _chocou = !_chocou;
+                                });
+                              },
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 24,
+                          ),
+                          Visibility(
+                            visible: !_chocou,
+                            child: Column(
+                              children: [
+                                Text('Qual o tempo restante da Reide?'),
+                                Slider(
+                                  value: _remainTimeOfRaid,
+                                  label: _remainTimeOfRaid.toInt() == 1 ? _remainTimeOfRaid.toInt().toString() + " minuto" : _remainTimeOfRaid.toInt().toString() + " minutos",
+                                  divisions: 45,
+                                  min: 1,
+                                  max: 45,
+                                  onChanged: (time) {
+                                    setState(() {
+                                      _remainTimeOfRaid = time;
+                                      _currentTime = DateTime.now();
+                                      _maxWaitingTimeForRaid = 1;
+                                    });
+                                  },
+                                ),
+                                Text(_remainTimeOfRaid.toInt() == 1 ? _remainTimeOfRaid.toInt().toString() + " minuto" : _remainTimeOfRaid.toInt().toString() + " minutos"),
+                                const SizedBox(
+                                  height: 24,
+                                ),
+                                Text('Até que horas você quer fazer a Reide?'),
+                                Slider(
+                                  value: _maxWaitingTimeForRaid,
+                                  label: _currentTime.add(Duration(minutes: _maxWaitingTimeForRaid.toInt())).hour.toString() + ':' + _currentTime.add(Duration(minutes: _maxWaitingTimeForRaid.toInt())).minute.toString(),
+                                  divisions: _remainTimeOfRaid.toInt(),
+                                  min: 1,
+                                  max: _remainTimeOfRaid,
+                                  onChanged: (time) {
+                                    setState(() {
+                                      _maxWaitingTimeForRaid = time;
+                                    });
+                                  },
+                                ),
+                                Text(_currentTime.add(Duration(minutes: _maxWaitingTimeForRaid.toInt())).hour.toString() + ':' + _currentTime.add(Duration(minutes: _maxWaitingTimeForRaid.toInt())).minute.toString()),
+                                const SizedBox(
+                                  height: 24,
+                                ),
+                              ],
+                            ),
+                            replacement: Column(
+                              children: [
+                                Text('nn chocou'),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          FlatButton(
+                            child: Text('Compartilhar'),
+                            color: Colors.red,
+                            onPressed: () {
+                              var today = new DateTime.now();
+                              var endOfRaid = today.add(Duration(minutes: _remainTimeOfRaid.toInt()));
+                              print(endOfRaid.hour.toString() + ':' + endOfRaid.minute.toString());
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
